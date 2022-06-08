0308e209ce0b59d9cc8049c48c9f4146362116f5
Omar Hernan Osorio Porras <omarhop1114@gmail.com>
2022-06-08 08:38:56 -0500

    solve console errors and responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,11 +4,11 @@ import {Link as LinkRouter} from "react-router-dom";
 function Card({key, city, country,image}) {
   return (
 		<LinkRouter to={"/detail/"+city}>
-			<div key={key} className="max-h-50 rounded-lg border shadow-md bg-orange-300 border-orange-700 m-2 hover:animate-pulse">
-						<img className="rounded-t-lg" src={image} alt="" />
+			<div key={key} className="max-h-50 rounded-lg border shadow-md bg-orange-300 border-orange-700 m-1 hover:animate-pulse">
+						<img className="rounded-t-lg w-full" src={image} alt="" />
 				<div className="md:p-2 p-0">
-					<h5 className="mb-2 text-base md:text-2xl font-bold tracking-tight text-center text-white">{city}</h5> 
-					<p className="mb-3 font-normal text-black text-center">{country}</p>
+					<h5 className="mb-0.5 md:mb-1 text-base md:text-2xl font-bold tracking-tight text-center text-white">{city}</h5> 
+					<p className="mb-0.5 md:mb-1 font-normal text-black text-center">{country}</p>
 				</div>
 			</div>
 		</LinkRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
 			<div className='backdrop-blur-sm bg-amber-500/30 mx-10 md:mx-20 rounded-lg'>
 				<h1 className='text-5xl md:text-8xl mb-3 font-bold'>MyTinerary</h1>
 				<p className='text-1xl md:text-4xl mb-4'>Find your perfect trip, designed by insiders who know and love their cities!</p>
-				<LinkRouter to="/cities" type="button" class="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 animate-bounce">Dare to discover</LinkRouter>
+				<LinkRouter to="/cities" type="button" className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 animate-bounce">Dare to discover</LinkRouter>
 			</div>
     </div>
 		<Carousel title="Popular MyTineraries" />
