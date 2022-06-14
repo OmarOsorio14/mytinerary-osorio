38f44fb1ea7926c628fa643f1fe30cf2b8078311
Omar Hernan Osorio Porras <omarhop1114@gmail.com>
2022-06-14 13:30:36 -0500

    add controllers an routes for cities

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 require('dotenv').config()
 require('./config/database')
 const express = require('express')
+const Router = require('./routes/cities')
 const app = express()
 const PORT = 4000
 
+//middlewares
+app.use(express.json())
+app.use('/api', Router)
 app.set('port',PORT)
-app.get('/', (req, res) => {
-    res.send('working!!!!!')
-})
 
 app.listen(PORT, () => {
     console.log('Server is running on: '+app.get('port'))
