4864c1da173765ead017228dd225b5b0f3d2922b
Omar Hernan Osorio Porras <omarhop1114@gmail.com>
2022-06-23 21:16:16 -0500

    add message when city doesn't have itineraries

--- frontend/src/pages/Detail.jsx
+++ frontend/src/pages/Detail.jsx
@@ -5,6 +5,8 @@ import { useParams } from 'react-router-dom'
 import cityActions from '../redux/actions/cityActions';
 import {useDispatch,useSelector} from 'react-redux'
 import Itinerary from '../components/Itinerary';
+import Alert from '../components/Alert';
+
 
 
 function Detail() {
@@ -56,14 +58,15 @@ function Detail() {
 					<p className=' text-5xl mb-2 text-left font-bold'>{city.name}</p>
 					<p className=' text-3xl mb-2 text-left font-bold'>Description</p>
 					<p className=' text-left'>{city.description}</p>
-					{cityLoad ? Object.keys(city.itineraries).map((itinerary) => {
+					{cityLoad ? Object.keys(city.itineraries).length ===0 ? <Alert type="info" title="this city doesn't have itineraries yet" message="At the moment we do not have any itinerary available for this city, maybe the next time that you comeback there is one waiting for you"/>
+					: Object.keys(city.itineraries).map((itinerary) => {
 						return <div className='pt-14' ref={refs[city.itineraries[itinerary]._id]} key={city.itineraries[itinerary]._id}><Itinerary id={city.itineraries[itinerary]._id} itinerary={city.itineraries[itinerary]}/></div> 
 						
 					}): ""}
 				</div>
-				<button className="ml-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-2 max-w-max max-h-max fixed inset-y-1/2 rounded animate-pulse" onClick={()=>{handleClose(!showSideBar)}}>
+				{cityLoad && Object.keys(city.itineraries).length !==0 ? <button className="ml-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-2 max-w-max max-h-max fixed inset-y-1/2 rounded animate-pulse" onClick={()=>{handleClose(!showSideBar)}}>
   				<ArrowRightIcon className='md:h-8 md:w-8 h-4 w-4' />
-				</button>
+				</button> : "" }
 				{showSideBar ? 	<SideBar handleClose={handleClose} handleScroll={handleScroll} city={city.name} itinerary={city.itineraries}/> : ""}
 			</>
 	)
